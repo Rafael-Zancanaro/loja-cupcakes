c03cdc76871b427fa1946dcd4be026b3a08c1dc3
Rafael <rafa10_zc@hotmail.com>
2023-10-18 12:42:55 -0300

    organizando corpo pagina cupcakes

--- app/cupcakes/page.tsx
+++ app/cupcakes/page.tsx
@@ -2,78 +2,99 @@ import "@/styles/cupcake.css"
 import Image from "next/image";
 import Menu from "@/components/menu"
 import Header from "@/components/header";
+import BotaoCarrinho from "@/components/botao-carrinho";
 
 export default function Cupcakes() {
   return (
-    <div className="div_cupcakes_principal">
+    <div>
       <Header />
-      <Menu />
-      <ul className="lista_cupcakes">
-        <li className="item">
-          <Image
-            src="/padaria-bolo.png"
-            alt="Logo Cake Shop"
-            width={180}
-            height={203}
-          />
-        </li>
-        <li className="item">
-          <Image
-            src="/padaria-bolo.png"
-            alt="Logo Cake Shop"
-            width={180}
-            height={203}
-          />
-        </li>
-        <li className="item">
-          <Image
-            src="/padaria-bolo.png"
-            alt="Logo Cake Shop"
-            width={180}
-            height={203}
-          />
-        </li>
-        <li className="item">
-          <Image
-            src="/padaria-bolo.png"
-            alt="Logo Cake Shop"
-            width={180}
-            height={203}
-          />
-        </li>
-        <li className="item">
-          <Image
-            src="/padaria-bolo.png"
-            alt="Logo Cake Shop"
-            width={180}
-            height={203}
-          />
-        </li>
-        <li className="item">
-          <Image
-            src="/padaria-bolo.png"
-            alt="Logo Cake Shop"
-            width={180}
-            height={203}
-          />
-        </li>
-        <li className="item">
-          <Image
-            src="/padaria-bolo.png"
-            alt="Logo Cake Shop"
-            width={180}
-            height={203}
-          />
-        </li>
-        <li className="item">
-          <Image
-            src="/padaria-bolo.png"
-            alt="Logo Cake Shop"
-            width={180}
-            height={203}
-          />
-        </li>
-      </ul>
+      <div className="cupcakes">
+        <Menu />
+        <ul className="lista_cupcakes">
+          <li className="item">
+            <Image
+              src="/padaria-bolo.png"
+              alt="Logo Cake Shop"
+              width={303}
+              height={280}
+            />
+            <p>R$ 4,99</p>
+            <BotaoCarrinho />
+          </li>
+          <li className="item">
+            <Image
+              src="/padaria-bolo.png"
+              alt="Logo Cake Shop"
+              width={303}
+              height={280}
+            />
+            <p>R$ 4,99</p>
+            <BotaoCarrinho />
+          </li>
+          <li className="item">
+            <Image
+              src="/padaria-bolo.png"
+              alt="Logo Cake Shop"
+              width={303}
+              height={280}
+            />
+            <p>R$ 4,99</p>
+            <BotaoCarrinho />
+          </li>
+          <li className="item">
+            <Image
+              src="/padaria-bolo.png"
+              alt="Logo Cake Shop"
+              width={303}
+              height={280}
+            />
+            <p>R$ 4,99</p>
+            <BotaoCarrinho />
+          </li>
+          <li className="item">
+            <Image
+              src="/padaria-bolo.png"
+              alt="Logo Cake Shop"
+              width={303}
+              height={280}
+            />
+            <p>R$ 4,99</p>
+            <BotaoCarrinho />
+          </li>
+          <li className="item">
+            <Image
+              src="/padaria-bolo.png"
+              alt="Logo Cake Shop"
+              width={303}
+              height={280}
+            />
+            <p>R$ 4,99</p>
+            <BotaoCarrinho />
+          </li>
+          <li className="item">
+            <Image
+              src="/padaria-bolo.png"
+              alt="Logo Cake Shop"
+              width={303}
+              height={280}
+            />
+            <p>R$ 4,99</p>
+            <BotaoCarrinho />
+          </li>
+          <li className="item">
+            <Image
+              src="/padaria-bolo.png"
+              alt="Logo Cake Shop"
+              width={303}
+              height={280}
+            />
+            <div className="footer_item">
+              <p>R$ 4,99</p>
+              <BotaoCarrinho />
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
