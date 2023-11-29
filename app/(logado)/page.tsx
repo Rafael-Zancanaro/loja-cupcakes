db75f362b7b4734f1c10b98112ce3b0c2c041427
Rafael <rafa10_zc@hotmail.com>
2023-11-29 00:47:13 -0300

    footer sobre

--- app/(logado)/page.tsx
+++ app/(logado)/page.tsx
@@ -1,11 +1,12 @@
 import Aside from "@/components/aside";
 import Image from "next/image";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 export default function Principal() {
   return (
     <div>
       <Aside />
-      <div className="sm:ml-64 min-h-screen grid gap-3 mt-14 p-4 max-w-full bg-gray-300">
+      <div className="sm:ml-64 grid gap-3 mt-14 p-4 max-w-full bg-gray-300">
         <div className="flex justify-center">
           <Image
             width={260}
@@ -63,6 +64,36 @@ export default function Principal() {
           </div>
         </div>
       </div>
+      <footer className="flex flex-col justify-center items-center bg-gray-400 h-36">
+        <p>Site Desenvolvido por Rafael Zancanaro Cardoso</p>
+        <p>Me siga nas Redes Sociais 👍</p>
+        <div className="flex gap-3">
+          <a
+            className="flex flex-row content-center items-center gap-1 bg-blue-400 rounded-md p-1"
+            href="https://github.com/Rafael-Zancanaro"
+            target="_blank"
+          >
+            <FaGithub />
+            Github
+          </a>
+          <a
+            className="flex flex-row content-center items-center gap-1 bg-blue-400 rounded-md p-1"
+            href="https://www.instagram.com/rafa_zancanaro/"
+            target="_blank"
+          >
+            <FaInstagram />
+            Instagram
+          </a>
+          <a
+            className="flex flex-row content-center items-center gap-1 bg-blue-400 rounded-md p-1"
+            href="https://www.linkedin.com/in/rafael-zancanaro-cardoso-0a1854215/"
+            target="_blank"
+          >
+            <FaLinkedin />
+            Linkedin
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
