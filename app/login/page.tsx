6eb7d1efcf2f5a3d241a0b60e9292a196ecb3594
Rafael <rafa10_zc@hotmail.com>
2023-11-30 00:34:14 -0300

    ajuste logo loja

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Form from "@/components/form";
 import Link from "next/link";
 
@@ -8,9 +7,8 @@ export default function Login() {
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
           <Link href="/">
-            <Image
-              src="/padaria-bolo.png"
-              priority
+            <img
+              src="..\principal\padaria-bolo.png"
               alt="Logo Cake Shop"
               className="h-20 w-20 rounded-full"
               width={20}
